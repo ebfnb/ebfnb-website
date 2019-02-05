9ff122881439a122900f312e421f1d6d6133188a
jspoet <skihappy17@gmail.com>
2019-02-04 20:22:13 -0800

    Adjusted styling on Eat with Us

--- src/components/EatPage.tsx
+++ src/components/EatPage.tsx
@@ -6,12 +6,10 @@ import PageTitle from './PageTitle'
 export const jsxFix = jsx
 
 const articleCss = css`
-  & + & {
-    margin-top: 2rem;
-    border-top: 2px dotted #ddd;
-    h3 {
-      margin-top: 0.5rem;
-    }
+  margin-top: 2rem;
+  border-top: 2px dotted #ddd;
+  h3 {
+    margin-top: 0.5rem;
   }
 `
 
@@ -20,6 +18,7 @@ const EatPage = () => (
     <PageTitle documentTitle="Eat with Us">
       Eat with East Bay Food Not Bombs
     </PageTitle>
+    <p>We serve six days a week, rain or shine.</p>
     <article css={articleCss}>
       <h3>Monday – Friday, 3:00pm</h3>
       <p>People’s Park, Berkeley</p>
